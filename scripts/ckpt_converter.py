08beb177b29763f93d3699685c986630a95be642
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-08-13 16:59:30 +0800

    Update ckpt_converter.py

--- scripts/ckpt_converter.py
+++ scripts/ckpt_converter.py
@@ -2,8 +2,8 @@ import torch
 from safetensors.torch import save_file
 
 
-CKPT_PATH = "./models/epoch=0-step=14253.ckpt"
-OUT_PATH = "G:/ComfyUI/models/checkpoints/hdm-xut-340M-1024px-note.safetensors"
+CKPT_PATH = "KBlueLeaf/HDM-xut-340M-anime/training_ckpt/768px.ckpt"
+OUT_PATH = "KBlueLeaf/HDM-xut-340M-anime/hdm-xut-340M-768px-note.safetensors"
 
 print("Loading ckpt...")
 ckpt = torch.load(CKPT_PATH, map_location="cpu", weights_only=False)
